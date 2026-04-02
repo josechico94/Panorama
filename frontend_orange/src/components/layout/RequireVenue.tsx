61e566a756c2ce49bc842806e4d59827f43ea4c5
Jose Chico <joseichico94@gmail.com>
2026-04-02 10:05:12 +0200

    fix: AuthPage TS errors

--- frontend_orange/src/components/layout/RequireVenue.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import { useVenueStore } from '@/store'
-export default function RequireVenue() {
-  const token = useVenueStore(s => s.token)
-  if (!token) return <Navigate to="/locale/login" replace />
-  return <Outlet />
-}
