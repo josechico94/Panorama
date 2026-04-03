10f36a09e5a415899ad79f2c8558cd7b6687ae57
Jose Chico <joseichico94@gmail.com>
2026-04-03 10:38:33 +0200

    fix: AppLayout no duplicate minHeight

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+﻿import { Outlet } from 'react-router-dom'
 import BottomNav from './BottomNav'
 import TopBar from './TopBar'
 import CouponSlider from '@/components/coupons/CouponSlider'
